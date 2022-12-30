d94a72dcff2f50f4406b8e8bcd432b014f7656e8
MartijnVanDeRee <martijn.ree.van.de@zilverenkruis.nl>
2022-12-30 17:51:04 +0100

    added cards

---
+++ components/Card.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card card-compact w-96 bg-base-100 shadow-xl">
+    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">Paard</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import Navbar from "../components/Navbar.vue";
+import Card from "../components/Card.vue";
 </script>
 
 <template>
   <Navbar />
+
+  <div class="grid sm:grid-cols-2 gap-4">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
 </template>
