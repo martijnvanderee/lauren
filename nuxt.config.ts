fa9e8564422a027e579f634e983b4d2f7af2f286
MartijnVanDeRee <martijn.ree.van.de@zilverenkruis.nl>
2023-01-05 16:38:08 +0100

    add identity netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({ modules: ["@nuxtjs/tailwindcss"] });
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"],
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
