d310d01b9aebe9e6f8c14e9df146a622e474a38a
MartijnVanDeRee <martijn.ree.van.de@zilverenkruis.nl>
2023-01-08 18:58:13 +0100

    added cloudinary

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
+  image: {
+    cloudinary: {
+      baseURL: "https://res.cloudinary.com/dta9vptzh/image/upload/",
+    },
+  },
 });
+
+
+
