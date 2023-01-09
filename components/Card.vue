592159520b70a7393ee35eaabfc87b59427ee720
MartijnVanDeRee <martijn.ree.van.de@zilverenkruis.nl>
2023-01-09 17:58:30 +0100

    routes

--- components/Card.vue
+++ components/Card.vue
@@ -3,7 +3,7 @@ export interface Props {
   title: string;
   description?: string;
   image: string;
-  price: string;
+  price: number;
 }
 
 withDefaults(defineProps<Props>(), {});
@@ -11,13 +11,21 @@ withDefaults(defineProps<Props>(), {});
 
 <template>
   <div class="card card-compact w-96 bg-base-100 shadow-xl">
-    <figure><img :src="image" /></figure>
+    <NuxtImg
+      provider="cloudinary"
+      :src="image"
+      width="300"
+      height="200"
+      :modifiers="{ effect: 'grayscale' }"
+    />
     <div class="card-body">
       <h2 class="card-title">{{ title }}</h2>
       <p>{{ description }}</p>
       <div class="card-actions justify-end">
         <span class="">{{ price }}</span>
-        <button class="btn btn-primary">Buy Now</button>
+        <NuxtLink :to="'/product/' + title">
+          <button class="btn btn-primary">Buy Now</button>
+        </NuxtLink>
       </div>
     </div>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 import type { QueryBuilderParams } from "@nuxt/content/dist/runtime/types";
 
 import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
 import Card from "../components/Card.vue";
 
 useHead({
@@ -19,60 +20,18 @@ const query: QueryBuilderParams = {
 <template>
   <Navbar />
 
-  <ContentList :query="query" v-slot="{ list }">
-    <div v-for="article in list" :key="article._path">
-      <h2>{{ article.title }}</h2>
-      <NuxtImg
-        provider="cloudinary"
-        :src="article.image"
-        width="30"
-        height="20"
-        :modifiers="{ roundCorner: 'max', effect: 'grayscale' }"
-      />
-    </div>
-  </ContentList>
-
   <div class="grid sm:grid-cols-2 gap-4">
-    <Card
-      title="this is a card"
-      description="If a dog chews shoes whose shoes does he choose?"
-      image="https://placeimg.com/400/225/arch"
-      price="13.99"
-    />
-
-    <Card
-      title="this is a card"
-      description="If a dog chews shoes whose shoes does he choose?"
-      image="https://placeimg.com/400/225/arch"
-      price="13.99"
-    />
-
-    <Card
-      title="this is a card"
-      description="If a dog chews shoes whose shoes does he choose?"
-      image="https://placeimg.com/400/225/arch"
-      price="13.99"
-    />
-
-    <Card
-      title="this is a card"
-      description="If a dog chews shoes whose shoes does he choose?"
-      image="https://placeimg.com/400/225/arch"
-      price="13.99"
-    />
-
-    <Card
-      title="this is a card"
-      description="If a dog chews shoes whose shoes does he choose?"
-      image="https://placeimg.com/400/225/arch"
-      price="13.99"
-    />
-
-    <Card
-      title="this is a card"
-      description="If a dog chews shoes whose shoes does he choose?"
-      image="https://placeimg.com/400/225/arch"
-      price="13.99"
-    />
+    <ContentList :query="query" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <Card
+          :title="article.title"
+          :description="article.discription"
+          :image="article.image"
+          :price="article.price"
+        />
+      </div>
+    </ContentList>
   </div>
+
+  <Footer />
 </template>
